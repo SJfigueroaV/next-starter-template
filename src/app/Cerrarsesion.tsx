a93670ae3172d4290c48c547063c109616390dd1
SJfigueroaV <syasaf3389@gmail.com>
2025-05-30 15:56:50 -0500

    Metadata

--- src/app/Cerrarsesion.tsx
+++ src/app/Cerrarsesion.tsx
@@ -6,50 +6,50 @@ import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export default function CerrarSesion() {
-  const router = useRouter();
-  const [user, setUser] = useState<any>(null);
+    const router = useRouter();
+    const [user, setUser] = useState<any>(null);
 
-  useEffect(() => {
-    // Obtener sesión actual
-    const getSession = async () => {
-      const {
-        data: { session },
-      } = await supabase.auth.getSession();
-      setUser(session?.user ?? null);
+    useEffect(() => {
+        // Obtener sesión actual
+        const getSession = async () => {
+            const {
+                data: { session },
+            } = await supabase.auth.getSession();
+            setUser(session?.user ?? null);
+        };
+
+        getSession();
+
+        // Escuchar cambios en la sesión
+        const { data: listener } = supabase.auth.onAuthStateChange((_event, session) => {
+            setUser(session?.user ?? null);
+        });
+
+        return () => {
+            listener?.subscription.unsubscribe();
+        };
+    }, []);
+
+    const handleSignOut = async () => {
+        await supabase.auth.signOut();
+        window.location.reload(); // recarga la página actual
     };
 
-    getSession();
 
-    // Escuchar cambios en la sesión
-    const { data: listener } = supabase.auth.onAuthStateChange((_event, session) => {
-      setUser(session?.user ?? null);
-    });
+    if (!user) return null; // No mostrar si no hay sesión
 
-    return () => {
-      listener?.subscription.unsubscribe();
-    };
-  }, []);
-
-  const handleSignOut = async () => {
-  await supabase.auth.signOut();
-  window.location.reload(); // recarga la página actual
-};
-
-
-  if (!user) return null; // No mostrar si no hay sesión
-
-  return (
-    <button
-      onClick={handleSignOut}
-      className="items-center justify-center hidden text-sm font-semibold text-white/80 md:flex group hover:text-yellow-300 gap-x-2"
-    >
-      <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 transition-transform group-hover:scale-125" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-        <path d="M10 8v-2a2 2 0 0 1 2 -2h7a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-7a2 2 0 0 1 -2 -2v-2"></path>
-        <path d="M15 12h-12l3 -3"></path>
-        <path d="M6 15l-3 -3"></path>
-      </svg>
-      <span className="pl-2 text-sm font-normal transition-colors">Cerrar sesión</span>
-    </button>
-  );
+    return (
+        <button
+            onClick={handleSignOut}
+            className="items-center justify-center hidden text-sm font-semibold text-white/80 md:flex group hover:text-yellow-300 gap-x-2"
+        >
+            <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 transition-transform group-hover:scale-125" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M10 8v-2a2 2 0 0 1 2 -2h7a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-7a2 2 0 0 1 -2 -2v-2"></path>
+                <path d="M15 12h-12l3 -3"></path>
+                <path d="M6 15l-3 -3"></path>
+            </svg>
+            <span className="pl-2 text-sm font-normal transition-colors">Cerrar sesión</span>
+        </button>
+    );
 }
--- src/app/Usuario.tsx
+++ src/app/Usuario.tsx
@@ -2,34 +2,34 @@
 import { useEffect, useState } from "react";
 import { supabase } from "@/supabaseClient";
 import Image from "next/image";
-import  Avatar  from "./avatar.jpg"
+import Avatar from "./avatar.jpg"
 export default function UserProfile() {
-  const [user, setUser] = useState<any>(null);
+    const [user, setUser] = useState<any>(null);
 
-  useEffect(() => {
-    supabase.auth.getUser().then(({ data }) => {
-      setUser(data.user);
-    });
-  }, []);
+    useEffect(() => {
+        supabase.auth.getUser().then(({ data }) => {
+            setUser(data.user);
+        });
+    }, []);
 
-  if (!user) return null;
+    if (!user) return null;
 
-  const avatar = user.user_metadata?.avatar_url;
-  const name = user.user_metadata?.full_name || user.email;
+    const avatar = user.user_metadata?.avatar_url;
+    const name = user.user_metadata?.full_name || user.email;
     console.log("Avatar URL:", avatar);
 
-  return (
-    
-      <div className="relative flex items-center justify-center group">
-        <img    
-         src={avatar}
-          alt="Avatar del usuario"
-          className="w-10 h-10 transition rounded-full"
-          width={40}
-          height={40}
-        />
-        <span className="pl-3 text-xl font-medium text-yellow-300">{name}</span>
-      </div>
-    
-  );
+    return (
+
+        <div className="relative flex items-center jSustify-center group">
+            <img
+                src={avatar}
+                alt="Avatar del usuario"
+                className="w-10 h-10 transition rounded-full"
+                width={40}
+                height={40}
+            />
+            <span className="pl-3 text-xl font-medium text-yellow-300">{name}</span>
+        </div>
+
+    );
 }
