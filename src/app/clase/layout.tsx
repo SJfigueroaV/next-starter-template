79c73ab6da5d02dd39e7acb321de1cb14ebe68cc
SJfigueroaV <syasaf3389@gmail.com>
2025-06-29 13:04:46 -0500

    prueba header subtemas

--- src/app/clase/layout.tsx
+++ src/app/clase/layout.tsx
@@ -93,21 +93,19 @@ export default function ClaseLayout({ children }: { children: React.ReactNode })
   return (
     <div>
       <header>
-        <h1>PRUEBA DE LAYOUT</h1>
-      </header>
-      
-      
+        <h3 className="fixed gap-x-2 h-20 w-full flex justify-start items-center top-0 mb-24 -mt-2 text-sm font-bold tracking-wider uppercase text-transform text-white/7 bg-[#13111C] z-10 m-0 left-0 md:left-auto pl-4 md:-ml-4"><a className="text-yellow-300 transition hover:contrast-125 hover:scale-105" href="/"><svg xmlns="http://www.w3.org/2000/svg" className="w-8 h-8 transition" width="40" height="40" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M12 8l-4 4l4 4"></path><path d="M16 12h-8"></path><path d="M12 3c7.2 0 9 1.8 9 9s-1.8 9 -9 9s-9 -1.8 -9 -9s1.8 -9 9 -9z"></path></svg></a><span className="text-white">Introducción</span></h3>
+      </header> 
       <main>
-      {children}
+        {children}
       </main>
-      <footer>  
-      <div>
-        Estado del subtema:{" "}
-        {estado === "completado" && <span style={{ color: "limegreen" }}>✔ Completado</span>}
-        {estado === "en_progreso" && <span style={{ color: "orange" }}>⏳ En progreso</span>}
-        {estado === "no_completado" && <span style={{ color: "red" }}>✖ No completado</span>}
-        {!estado && <span>Cargando estado...</span>}
-      </div>
+      <footer>
+        <div>
+          Estado del subtema:{" "}
+          {estado === "completado" && <span style={{ color: "limegreen" }}>✔ Completado</span>}
+          {estado === "en_progreso" && <span style={{ color: "orange" }}>⏳ En progreso</span>}
+          {estado === "no_completado" && <span style={{ color: "red" }}>✖ No completado</span>}
+          {!estado && <span>Cargando estado...</span>}
+        </div>
       </footer>
     </div>
   );
